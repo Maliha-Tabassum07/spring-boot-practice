d7d2514aefe30bcf531977283c00d6ad09cd0779
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-03-29 17:33:51 +0600

    record

--- spring-boot-example/src/main/java/com/maliha/springbootexample/controller/Controller.java
+++ spring-boot-example/src/main/java/com/maliha/springbootexample/controller/Controller.java
@@ -3,6 +3,8 @@ package com.maliha.springbootexample.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class Controller {
     @GetMapping("/")
@@ -18,5 +20,40 @@ public class Controller {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet){}
+    //record GreetResponse(String greet){}
+
+
+
+    //equivalent of record:
+    public class GreetResponse{
+        public final String greet;
+
+        public GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+    }
 }
