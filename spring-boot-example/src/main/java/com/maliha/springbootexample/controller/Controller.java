82a0801efab4fc977887e30a80868f287b2ebea3
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-05-10 17:21:46 +0600

    model

--- spring-boot-example/src/main/java/com/maliha/springbootexample/controller/Controller.java
+++ spring-boot-example/src/main/java/com/maliha/springbootexample/controller/Controller.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 public class Controller {
@@ -18,9 +17,9 @@ public class Controller {
     }
     @GetMapping("/greet/record")
     public GreetResponse greetResponse(){
-        return new GreetResponse("Hello",List.of("Java","Python","C++"),new Person("Person1"));
+        return new GreetResponse("Hello",List.of("Java ","Python","C++"),new Person("Person1", 30, 30000));
     }
-    record Person(String name){}
+    record Person(String name, int age,double savings){}
     record GreetResponse(String greet, List<String> favProgrammingLanguage,Person person){}
 
 
