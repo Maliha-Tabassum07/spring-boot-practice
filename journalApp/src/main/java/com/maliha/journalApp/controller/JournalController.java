8529dcf8720cd05f250345ec06e5c1401c4bdabe
Maliha-Tabassum07 <maliha.tabassum2@g.bracu.ac.bd>
2025-05-29 14:20:52 +0600

    Basic Apis for only for internal memory

---
+++ journalApp/src/main/java/com/maliha/journalApp/controller/JournalController.java
@@ -0,0 +1,43 @@
+package com.maliha.journalApp.controller;
+
+import com.maliha.journalApp.entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalController {
+    private Map<Long, JournalEntity> journalEntity= new HashMap<>();
+    @GetMapping
+    public Map<Long, JournalEntity> getAllJournalEntity() {
+        return journalEntity;
+    }
+    @PostMapping
+    public Boolean createJournalEntity(@RequestBody JournalEntity journalEntity) {
+        this.journalEntity.put(journalEntity.getId(), journalEntity);
+        return true;
+    }
+    @GetMapping("/{id}")
+    public JournalEntity getJournalEntityById(@PathVariable Long id) {
+        return journalEntity.get(id);
+    }
+    @PutMapping("/{id}")
+    public Boolean updateJournalEntity(@PathVariable Long id, @RequestBody JournalEntity journalEntity) {
+        if (this.journalEntity.containsKey(id)) {
+            this.journalEntity.put(id, journalEntity);
+            return true;
+        }
+        return false;
+    }
+    @DeleteMapping("/{id}")
+    public Boolean deleteJournalEntity(@PathVariable Long id) {
+        if (this.journalEntity.containsKey(id)) {
+            this.journalEntity.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+}
