fe28f041425fd1719a8f52baa7ab03582aa7c917
Maliha-Tabassum07 <maliha.tabassum2@g.bracu.ac.bd>
2025-06-30 13:38:00 +0600

    changes

--- journalApp/src/main/java/com/maliha/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/maliha/journalApp/entity/JournalEntry.java
@@ -4,11 +4,14 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Data
-@Document
+@Document(collection = "journal_entries")
 public class JournalEntry {
     @Id
     private String id;
     private String title;
     private String content;
+    private Date date;
 }
